42effcd17eedce8bfe7d4e80a433c22c4cba15bb
LokeshJavaDev <lokeshchhara.dev@gmail.com>
2026-03-17 23:20:39 +0530

    Added stack based problem

---
+++ src/Stack/ValidParentheses.java
@@ -0,0 +1,28 @@
+package Stack;
+
+import java.util.*;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (map.containsKey(ch)) {
+                if (stack.isEmpty() || stack.pop() != map.get(ch)) {
+                    return false;
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
